f4cb689e57b2bf6add53dbeb1206bb093b9e722a
RMCoder198 <rsmawari198@gmail.com>
2019-04-20 00:13:04 +0530

    finalize repo

--- server/server.js
+++ server/server.js
@@ -35,10 +35,10 @@ app.use("/api/posts", posts);
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("../build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
   });
 }
 
