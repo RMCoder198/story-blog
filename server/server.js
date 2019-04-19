4ec24548555efef390c1cfe679575d75c89e8fea
RMCoder198 <rsmawari198@gmail.com>
2019-04-20 01:37:38 +0530

    remove unwanted files

--- server/server.js
+++ server/server.js
@@ -35,10 +35,10 @@ app.use("/api/posts", posts);
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("../build"));
+  app.use(express.static(path.join(__dirname,"../build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build", "index.html"));
+    res.sendFile(path.join(__dirname, "../build/index.html"))
   });
 }
 
